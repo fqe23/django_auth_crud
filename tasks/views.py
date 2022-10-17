a8caded0a616ac00d957d54fcbec8b39f6399bcb
Fabian Quintero Echeverry <42185075+fqe23@users.noreply.github.com>
2022-10-17 08:34:27 -0500

    second commit

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,3 @@
-from tkinter import N
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
